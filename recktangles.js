6a78e0df4ba707e990c0456d21d5fe963b66bc77
sdlambert <schm01978@gmail.com>
2016-07-23 10:25:03 -0400

    Added description and link to Recktangles solution

--- recktangles.js
+++ recktangles.js
@@ -1,3 +1,37 @@
+// https://redd.it/4tetif
+
+// Input description
+
+// The input is a string word, a width and a height
+
+// Output description
+
+// Quality rektangles. See examples. Any orientation of the rektangle is acceptable
+// Examples
+
+//     Input: "REKT", width=1, height=1
+
+//     Output:
+
+//     R E K T
+//     E     K
+//     K     E
+//     T K E R
+
+//     Input: "REKT", width=2, height=2
+
+//     Output:
+
+//     T K E R E K T
+//     K     E     K
+//     E     K     E
+//     R E K T K E R
+//     E     K     E
+//     K     E     K
+//     T K E R E K T
+
+
+
 function makeRecktangles (str, width, height) {
 	var grid = [],
 	    strArr = str.split(""),
